99ddc78738d2656d39d64a97e6f1e555f1bad8a3
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-05-10 01:55:41 +0200

    Add example test

---
+++ src/test/java/ch/epfl/cs107/play/ExampleTest.java
@@ -0,0 +1,13 @@
+package ch.epfl.cs107.play;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+
+    @Test
+    void test(){
+        assertEquals(2, 1 + 1);
+    }
+}
